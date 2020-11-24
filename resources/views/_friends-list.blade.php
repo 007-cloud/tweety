33f28899446251df87662f5437625d9fd5d1e5ff
007-cloud <balginbek.balgin97@gmail.com>
2020-11-25 04:02:20 +0600

    Created friends list for home page

---
+++ resources/views/_friends-list.blade.php
@@ -0,0 +1,17 @@
+<h3 class="font-bold text-xg mb-4">Friends</h3>
+
+<ul>
+    @foreach (range(1, 8) as $index)
+        <li class="mb-4">
+            <div class="flex items-center text-sm">
+                <img
+                    src="https://i.pravatar.cc/40"
+                    alt=""
+                    class="rounded-full mr-2"
+                >
+
+                Duisen Balgynbek
+            </div>
+        </li>
+    @endforeach
+</ul>
