a1921fd5847d0155c7c5ae91cecfdda34aad09cd
007-cloud <balginbek.balgin97@gmail.com>
2020-11-25 04:05:35 +0600

    Created post section for home page

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,16 @@
+<div class="flex p-4 border-b border-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+    <div class="">
+        <h5 class="font-bold mb-4">Duisen Balgynbek</h5>
+        <p class="text-sm">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae omnis beatae minus modi culpa! Corrupti accusantium nesciunt, temporibus velit ea molestiae facilis? Quaerat harum quibusdam animi sunt exercitationem eum expedita.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quae reiciendis maiores fugiat earum a ducimus dolores expedita nam sunt.
+        </p>
+    </div>
+</div>
